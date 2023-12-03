5650f0097596409425d158db7c88988e13953860
gregmoudy <gregmoudy@gmail.com>
2023-12-03 11:31:29 -0600

    Day 03 Solved

--- main.py
+++ main.py
@@ -6,10 +6,17 @@
 # Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is 
 # unlocked when you complete the first. Each puzzle grants one star. Good luck!
 
-import day_01
-import day_02
+import glob
+import os
+
+# Get all the day_<int>.py files and import them.
+DAY_PYTHON_FILES = glob.glob('./day_*.py')
+DAY_MODULE_NAMES = [os.path.splitext(os.path.basename(x))[0] for x in DAY_PYTHON_FILES]
+for day_module_name in DAY_MODULE_NAMES:
+    exec(f'import {day_module_name}')
 
 
 if __name__ == '__main__':
-    day_01.run()
-    day_02.run()
+    # Run each day solver to view the output and timings.
+    for day_module_name in DAY_MODULE_NAMES:
+        eval(f'{day_module_name}.run()')
--- solver.py
+++ solver.py
@@ -10,6 +10,7 @@ class Solver:
         self._file_input_sample_1 = os.path.join(dirname, basename + '_input_sample_1.txt')
         self._file_input_sample_2 = os.path.join(dirname, basename + '_input_sample_2.txt')
         self._file_input = os.path.join(dirname, basename + '_input.txt')
+
         self._title = basename.title().replace('_', ' ')
 
 
@@ -27,11 +28,9 @@ class Solver:
     def run(self):
         start_time = timeit.default_timer()
 
-        print('\n')
         print('================================================================================')
         print(self._title)
-
-        print('\n')
+        print('================================================================================')
 
         # Part 1 Sample
         if not os.path.exists(self._file_input_sample_1):
@@ -44,11 +43,15 @@ class Solver:
             print(f'Part 1 Sample: {answer_1_sample}')
 
         # Part 2 Sample
-        if not os.path.exists(self._file_input_sample_2):
-            print(f'No sample input file 2 found: {self._file_input_sample_2}')
+        file_input_sample_2 = self._file_input_sample_2
+        if not os.path.exists(file_input_sample_2):
+            # Fallback to using the sample 1 input, most of the time it is reused but not always.
+            file_input_sample_2 = self._file_input_sample_1
+            if not os.path.exists(file_input_sample_2):
+                print(f'No sample input file 2 found: {self._file_input_sample_2}')
 
-        else:
-            data = self.read_input(self._file_input_sample_2)
+        if os.path.exists(file_input_sample_2):
+            data = self.read_input(file_input_sample_2)
 
             answer_2_sample = self.get_answer(data, part2 = True)
             print(f'Part 2 Sample: {answer_2_sample}')
@@ -69,5 +72,5 @@ class Solver:
 
         print('\n')
         print('Runtime: {} seconds.'.format(round(timeit.default_timer() - start_time, 5)))
-        print('================================================================================')
+        #print('================================================================================')
         print('\n')
